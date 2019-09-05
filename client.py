182a85cd9187b0d15ad014202fa82a56b236e5c4
Your Name <you@example.com>
2019-09-05 10:44:13 +0800

    d

--- client.py
+++ client.py
@@ -41,21 +41,26 @@ def on_message(client, userdata, msg):
     userdata.playlists = temp['data']
 
 def on_connect(client, userdata, flags, rc):
-    print "headset: %s Connected: %s "%(userdata.uuid, str(rc))
-    client.publish(Headset.Publish%(userdata.uuid), OnLine(userdata), 2)
+    if rc == 0:
+        print "headset: %s Connected "%(userdata.uuid)
+        client.publish(Headset.Publish%(userdata.uuid), OnLine(userdata), 2)
+        client.subscribe(Headset.Subscribe%(userdata.uuid), qos=2)
+    else:
+        userdata.connect_error = True
+        if userdata.viewerfunc != None:
+            userdata.viewerfunc(userdata._x, "wifi", userdata.level, userdata.sd, "连接错误", "red")
 
 class Headset(threading.Thread):
 
     HOST = "mqtt.smartheadset.qctchina.top"
     #HOST = "localhost"
     PORT = 1883
-    Subscribe = "/hardware/from/server/%s"
-    Publish = "/hardware/from/client/%s"
+    Subscribe = "hardware/from/server/%s"
+    Publish = "hardware/from/client/%s"
 
 
     def __init__(self, x):
         threading.Thread.__init__(self)
-        self._mqtt = mqtt.Client(userdata=self)
         self._uuid =  "123456123456"
         self._sd = 4 #4GB
         self._level = 0 
@@ -64,6 +69,7 @@ class Headset(threading.Thread):
         self._x = x
         self.viewerfunc = None
         self.total_download = 0
+        self.connect_error  = False
 
     @property
     def uuid(self):
@@ -93,29 +99,34 @@ class Headset(threading.Thread):
     def wifi(self, val):
         self._wifi = val
 
+    def __getPassword(self):
+        md5 = hashlib.md5()
+        md5.update(self.uuid + "qctchina")
+        password = md5.hexdigest()
+        return password
 
     def run(self):
         if self.viewerfunc != None:
             self.viewerfunc(self._x, "wifi",self._level,self._sd,"started", "blue")
-        md5 = hashlib.md5()
-        md5.update(self._uuid + "qcchina")
-        password = md5.hexdigest()
-        print password
-        self._mqtt.username_pw_set(self._uuid, password)
+
+        self._mqtt = mqtt.Client(client_id = self.uuid, userdata=self)
+        password = self.__getPassword()
+        self._mqtt.username_pw_set(self.uuid, password)
         self._mqtt.on_connect = on_connect
         self._mqtt.on_message = on_message
         self._mqtt.connect(Headset.HOST, Headset.PORT, 60)
-        self._mqtt.subscribe(Headset.Subscribe%(self._uuid), qos=2)
         self._mqtt.loop_start()
         if self.viewerfunc != None:
-            self.viewerfunc(self._x, "wifi",self._level,self._sd,"connected", "yellow")
+            self.viewerfunc(self._x, "wifi",self.level,self.sd,"connected", "yellow")
         while True:
            time.sleep(3)
+           if self.connect_error :
+              continue 
            print "{0}  wait for mqtt".format(self.uuid)
            for playlist in self.playlists:
-               self.total_download += download(playlist['download_url'], self._uuid)
+               self.total_download += download(playlist['download_url'], self.uuid)
                if self.viewerfunc != None:
-                  avaiable = self._sd - self.total_download/1024/1024/1024
+                  avaiable = self.sd - self.total_download/1024/1024/1024
                   self.level -= 1
                   self.viewerfunc(self._x, "wifi", self.level, avaiable, "downloading", "green")
 
@@ -128,7 +139,7 @@ class Headset(threading.Thread):
 if __name__ == '__main__':
     d = Headset(1)
     d.uuid = "123456123456"
-    d.wifi = 2
+    d.wifi = "4G"
     d.level = 20
     d.sd = 8
     d.start()
