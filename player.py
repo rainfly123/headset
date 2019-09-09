7e2d59186b8de426de0f3c78040fe3010429b37e
Your Name <you@example.com>
2019-09-09 17:55:18 +0800

    D

--- player.py
+++ player.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python
 #coding:utf8
 import os
-import subprocess
+import time
+import commands
  
 def  getfiles(uuid):
     path = os.path.join("data", uuid)
     files = os.listdir(path)
     return files
 
-def play(files)
-    path = os.path.join("data", files)
-    cmd = ["ffplay", path]
-    subprocess
+def play(uuid, files):
+    path = os.path.join("data", uuid, files)
+    cmd = " ".join(["ffplay", path])
+    start = time.time()
+    commands.getstatusoutput(cmd)
+    end = time.time()
+    basename = os.path.basename(path)
+    resourceid = os.path.splitext(basename)[0]
+    return  int(resourceid), int(end - start)
 
 if __name__ == "__main__":
-    print getfiles("123456123456")
-    a = raw_input()
-    print a ,type(a)
+    uuid = "123456123456"
+    files = getfiles(uuid)
+    print play(uuid, files[1])
+
+    import time
+    time.sleep(3)
